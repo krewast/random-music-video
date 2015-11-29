dbc7ab897eeaa4116120b51803b83518259eb0ba
krewast <bastiankres@gmail.com>
2015-11-29 13:27:31 +0100

    Added routes and 404

--- index.php
+++ index.php
@@ -8,26 +8,30 @@ $f3->set('DEBUG', 1);
 // Load configuration
 $f3->config('config.ini');
 
-$f3->route('GET /',
-	function($f3) {
-		// Go to random video
-	}
-);
+// Routes
+$f3->route('GET /', function($f3) {
+	// Go to random video
+});
 
-$f3->route('GET /v/@videoid',
-	function($f3) {
-		$videoid = $params['videoid'];
+$f3->route('GET /v/@videoid', function($f3, $params) {
+	$videoid = (string)$params['videoid'];
 
+	if (strlen($videoid) == 11) {
 		$f3->set('content','video.html');
 		echo View::instance()->render('layout.htm');
+	} else {
+		$f3->error(404);
 	}
-);
+});
 
-$f3->route('GET /about',
-	function($f3) {
-		$f3->set('content','about.html');
-		echo View::instance()->render('layout.htm');
-	}
-);
+$f3->route('GET /about', function($f3) {
+	$f3->set('content','about.html');
+	echo View::instance()->render('layout.htm');
+});
+
+$f3->set('ONERROR', function($f3) {
+	$f3->set('content','404.html');
+	echo View::instance()->render('layout.htm');
+});
 
 $f3->run();

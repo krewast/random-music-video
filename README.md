2376290299091e6bbfdea9239528340638947668
krewast <bastiankres@gmail.com>
2015-11-29 19:04:57 +0100

    Added Foundation Framework and video id data. Implemented design and functionality.

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 #Random music video
+
+Site can be found [here](http://www.krebas.com/random-music-video/)
+
+##Credit:
+
+- [Fat-Free Framework](http://fatfreeframework.com/)
+- [Foundation Framework](http://foundation.zurb.com/)
+- [Logo](https://thenounproject.com/icon/38847/) by [Víctor Fernández](https://thenounproject.com/vctrfrnndz/). Released under the following license: [CC BY 3.0](http://creativecommons.org/licenses/by/3.0/)
+
+##Further links
+
+- [Youtube iframe API reference](https://developers.google.com/youtube/iframe_api_reference)
--- index.php
+++ index.php
@@ -3,35 +3,40 @@
 // Kickstart the framework
 $f3 = require('lib/base.php');
 
-$f3->set('DEBUG', 1);
-
 // Load configuration
 $f3->config('config.ini');
 
 // Routes
 $f3->route('GET /', function($f3) {
-	// Go to random video
+	$videoIds = file('data/videoids.csv');
+	$randomVideoId = $videoIds[array_rand($videoIds)];
+
+	$f3->reroute($randomVideoId);
 });
 
-$f3->route('GET /v/@videoid', function($f3, $params) {
-	$videoid = (string)$params['videoid'];
+$f3->route('GET /@videoId', function($f3, $params) {
+	$videoId = (string)$params['videoId'];
 
-	if (strlen($videoid) == 11) {
-		$f3->set('content','video.html');
-		echo View::instance()->render('layout.htm');
+	if (strlen($videoId) == 11) {
+		$f3->set('videoId', $videoId);
+		$f3->set('title', $videoId);
+		$f3->set('content', 'video.html');
+		echo View::instance()->render('layout.html');
 	} else {
 		$f3->error(404);
 	}
 });
 
 $f3->route('GET /about', function($f3) {
-	$f3->set('content','about.html');
-	echo View::instance()->render('layout.htm');
+	$f3->set('title', 'About');
+	$f3->set('content', 'about.html');
+	echo View::instance()->render('layout.html');
 });
 
 $f3->set('ONERROR', function($f3) {
-	$f3->set('content','404.html');
-	echo View::instance()->render('layout.htm');
+	$f3->set('title', 'Error');
+	$f3->set('content', '404.html');
+	echo View::instance()->render('layout.html');
 });
 
 $f3->run();
